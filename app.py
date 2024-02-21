12420e16c3ec377355dfa79284479e0cdcd853bf
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-21 15:25:29 +0100

    finishing gTTs function

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from tkinter import *
 
 
 def pronounce():
-    pass
+    text = entry.get()
+    language = 'en'
+    audio_output = gTTS(text=text, lang=language, slow=False)
+    audio_output.save('app_audio.mp3')
+    os.system("start app_audio.mp3")
 
 
 root = Tk()
