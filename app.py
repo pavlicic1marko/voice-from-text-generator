d999142420fecf1b00ef986cf8c9d7daee5f96f4
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-21 15:17:39 +0100

    finishing UI

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from gtts import gTTS
 import os
 from tkinter import *
 
+
+def pronounce():
+    pass
+
+
 root = Tk()
+canvas = Canvas(root, width=400, height=300)
+canvas.pack()
+
+entry = Entry(root)
+canvas.create_window(200, 180, window=entry)
+
+button = Button(text="Start", command=pronounce)
+canvas.create_window(200, 230, window=button)
 
 root.mainloop()
