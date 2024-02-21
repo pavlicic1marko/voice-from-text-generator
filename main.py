4e4c552a9433fe6170a4f74fc0dc728dd8d0b39f
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-21 12:15:46 +0100

    convert text to speach and save it to mp3 file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from gtts import gTTS
+import os
+
+text = "LOL say something about chat gpt"
+audio_output = gTTS(text=text, lang='en', slow=False)  # slow is the speed of speach
+audio_output.save('audio.mp3')
+
+#start command is for windows os to play the amp3 file, for mac is afplay
+os.system("start audio.mp3")
