eb4f82f9cdb1fa3f8a51bbe504528380dc741947
Marko Pavlicic <marko.pavlicic@computerrock.com>
2024-02-23 15:41:03 +0100

    adding commands for creating audio and playing it

--- web_app.py
+++ web_app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+from gtts import gTTS
+import os
 
 app = Flask(__name__, template_folder="templates")
+
+
+def create_audio(text):
+    text = text
+    audio_output = gTTS(text=text, lang='en', slow=False)  # slow is the speed of speach
+    audio_output.save('audio.mp3')
+
+
+def play_audio():
+    os.system("start audio.mp3")
 
 
 @app.route("/")
@@ -11,8 +23,10 @@ def index():
 @app.route("/generate", methods=["POST"])
 def generate():
     print("this is a test")
+    text = request.form['text']
+    create_audio(text)
+    play_audio()
     return render_template("index.html")
-
 
 
 if __name__ == '__main__':
